8d83dc397f243380aaa34d58f717cea6a68e4c9d
Bernhard Schmitt <b.schmitt@core4.de>
2017-06-12 17:50:15 +0200

    TASK: Apply migration Neos.Neos-20161125002322
    Adjusts code to package renaming from "TYPO3.Neos" to "Neos.Neos"

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
@@ -55,7 +55,7 @@ class NodeIndexCommandController extends CommandController
 
     /**
      * @Flow\Inject
-     * @var \TYPO3\Neos\Domain\Service\ContentDimensionPresetSourceInterface
+     * @var \Neos\Neos\Domain\Service\ContentDimensionPresetSourceInterface
      */
     protected $contentDimensionPresetSource;
 
