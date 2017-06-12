95e70e5de9e9fa52c8cb4200444762a7c13ea8dd
Bernhard Schmitt <b.schmitt@core4.de>
2017-06-12 17:50:15 +0200

    TASK: Apply migration Neos.Fusion-20161125013710
    Adjusts code to package renaming from "TYPO3.TypoScript" to
    "Neos.Fusion"

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
@@ -783,9 +783,9 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
      *
      * Example Usage:
      *
-     *   searchFilter = TYPO3.TypoScript:RawArray {
+     *   searchFilter = Neos.Fusion:RawArray {
      *      author = 'Max'
-     *      tags = TYPO3.TypoScript:RawArray {
+     *      tags = Neos.Fusion:RawArray {
      *        0 = 'a'
      *        1 = 'b'
      *      }
@@ -847,8 +847,8 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
      *
      * Example Usage to create a terms aggregation for a property color:
      *
-     * aggregationDefinition = TYPO3.TypoScript:RawArray {
-     *   terms = TYPO3.TypoScript:RawArray {
+     * aggregationDefinition = Neos.Fusion:RawArray {
+     *   terms = Neos.Fusion:RawArray {
      *     field = "color"
      *   }
      * }
@@ -940,9 +940,9 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
      *
      * Example Usage of a term suggestion for the fulltext search
      *
-     * suggestionDefinition = TYPO3.TypoScript:RawArray {
+     * suggestionDefinition = Neos.Fusion:RawArray {
      *     text = "some text"
-     *     terms = TYPO3.TypoScript:RawArray {
+     *     terms = Neos.Fusion:RawArray {
      *         field = "body"
      *     }
      * }
